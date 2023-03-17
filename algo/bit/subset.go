31a345f8462c3f75ff3c85532f550876c546c0a7
xiew.xie <1280329598@qq.com>
2023-03-17 16:55:57 +0800

    利用位运算寻找子集

---
+++ algo/bit/subset.go
@@ -0,0 +1,17 @@
+package bit
+
+// 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）
+// 可以DFS，这里利用位运算巧妙实现
+func subsets(nums []int) [][]int {
+	var ret [][]int
+	for mark := 0; mark < 1<<len(nums); mark++ {
+		var subset []int
+		for i, val := range nums {
+			if mark>>i&1 == 1 {
+				subset = append(subset, val)
+			}
+		}
+		ret = append(ret, subset)
+	}
+	return ret
+}
